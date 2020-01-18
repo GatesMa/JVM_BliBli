e634dea62f75cf05917aa8336440f06f8fdf9750
GatesMa <gatesma@foxmail.com>
2020-01-18 20:54:27 +0800

    UPD JVM

--- src/com/gatesma/chapter3/TestGC.java
+++ src/com/gatesma/chapter3/TestGC.java
@@ -12,20 +12,26 @@ import com.sun.tools.javac.Main;
  */
 public class TestGC {
 
-    private Object instance;
-
-    public TestGC() {
-        byte[] b = new byte[2 * 1024 * 1024];
-    }
+//    private Object instance;
+//
+    public static final int _1MB = 1024 * 1024;
+//    public TestGC() {
+//        byte[] b = new byte[2 * 1024 * 1024];
+//    }
 
     public static void main(String[] args) {
-        TestGC m1 = new TestGC();
-        TestGC m2 = new TestGC();
-        m1.instance = m2;
-        m2.instance = m1;
-        m1 = null;
-        m2 = null;
+//        TestGC m1 = new TestGC();
+//        TestGC m2 = new TestGC();
+//        m1.instance = m2;
+//        m2.instance = m1;
+//        m1 = null;
+//        m2 = null;
+        byte[] a1, a2, a3, a4;
+        a1 = new byte[2 * _1MB];
+        a2 = new byte[2 * _1MB];
+        a3 = new byte[2 * _1MB];
+        a4 = new byte[4 * _1MB];
 //        System.gc();
-        
+
     }
 }
